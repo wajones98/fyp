4ef1ecc7893875eee275e808705f23bfd00b64cb
wjones98 <38158543+wjones98@users.noreply.github.com>
2020-02-09 00:20:11 +0000

    Upload File to S3 functionality

--- app.py
+++ app.py
@@ -1,9 +1,16 @@
 from flask import Flask, render_template, redirect, request, session, url_for
 from routes.upload_data import upload_data
+
 app = Flask(__name__)
 app.debug = True
 
 app.register_blueprint(upload_data)
+
+#UPLOAD_FOLDER = 'C:\\Users\\wajon\\OneDrive\\Documents\\GitHub\\fyp'
+#app.secret_key = b'\x9c\xabH.\x96\x00\xd12\xf4\xab\xc0\xe6\xf8\x19P\x15'
+#app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+#app.config['MAX_CONTENT_LENGTH'] = 16 * 1024 * 1024
+
 
 @app.route('/')
 def main():
--- routes/upload_data.py
+++ routes/upload_data.py
@@ -1,7 +1,26 @@
-from flask import Blueprint, render_template
+import boto3
+import os
+from flask import Blueprint, render_template, request
+from models import File as file_model
 
 upload_data = Blueprint('upload_data', __name__)
+
 
 @upload_data.route('/upload')
 def upload_page():
     return render_template('upload_data.html')
+
+
+@upload_data.route('/upload/submit', methods=['GET', 'POST'])
+def submit_files():
+    if request.method == 'POST':
+
+        data_files = request.files.getlist('file[]')
+
+        for data_file in data_files:
+            file = file_model.File(data_file)
+            file.upload_file_landing()
+            file.upload_file_s3()
+            file.delete_file_landing()
+
+        return render_template('upload_data.html')
